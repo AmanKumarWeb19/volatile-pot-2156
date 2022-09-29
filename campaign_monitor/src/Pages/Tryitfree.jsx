2c121031552e18f46ae0ad587a7b46e3f95c331a
AmanKumarWeb19 <akgupta0524@gmail.com>
2022-09-30 00:32:26 +0530

    login-signup-pages

--- campaign_monitor/src/Pages/Tryitfree.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Tryitfree = () => {
-  return <div>Try it free</div>;
-};
-
-export default Tryitfree;
--- campaign_monitor/src/components/Header.jsx
+++ campaign_monitor/src/components/Header.jsx
@@ -1,33 +1,38 @@
-import React, { useState } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 import Navbar from "./Navbar";
 import styled from "styled-components";
 
-const Header = () => {
-  const [loginTheme, setLoginTheme] = useState(false);
+const Header = (props) => {
+  console.log("propd", props.loginTheme);
   const MainHeader = styled.header`
-  /* padding: 0 4.8rem; */
-  height: 8rem;
-  background-color: ${loginTheme ? ({ theme }) => theme.colors.white : ({ theme }) => theme.colors.black};
-  /* border: 1px solid red; */
-  display: flex;
-  align-items: center;
-  .container {
-    width: 100%;
+    /* padding: 0 4.8rem; */
+    height: 8rem;
+    background-color: ${props.loginTheme
+      ? ({ theme }) => theme.colors.white
+      : ({ theme }) => theme.colors.black};
+    /* border: 1px solid red; */
     display: flex;
-    /* border: 1px solid orange; */
-    justify-content: end;
     align-items: center;
-  }
-  .logo {
-    height: auto;
-    /* max-width: 30%; */
-  }
-`;
+    .container {
+      width: 100%;
+      display: flex;
+      /* border: 1px solid orange; */
+      justify-content: end;
+      align-items: center;
+    }
+    .logo {
+      height: auto;
+      /* max-width: 30%; */
+    }
+  `;
   return (
     <MainHeader>
       <div className="container">
-        <Navbar loginTheme={loginTheme} setLoginTheme={setLoginTheme} />
+        <Navbar
+          loginTheme={props.loginTheme}
+          setLoginTheme={props.setLoginTheme}
+        />
       </div>
     </MainHeader>
   );
